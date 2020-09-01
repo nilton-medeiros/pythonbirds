9c20c3d1d627caee85ef40364fb5f8dfc9c46c8a
nilton-medeiros <nilton@sistrom.com.br>
2020-09-01 18:06:47 -0300

    Complementando a classe Pessoa init self.idade e self.nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=56):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return 'Olá'
+        return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Nilton')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Nilton G. Medeiros'
+    print(p.nome)
