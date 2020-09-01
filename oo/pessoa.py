8df31c5d3785daf9f9eb6956b2c797b85df7bf70
nilton-medeiros <nilton@sistrom.com.br>
2020-09-01 17:15:41 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,10 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Olá'
 
 
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
