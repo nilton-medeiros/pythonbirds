d29beb2791e62bf143562b1f0594664cea575a9f
nilton-medeiros <nilton@sistrom.com.br>
2020-09-02 09:40:18 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=56):
+    def __init__(self, *filhos, nome=None, idade=56):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Nilton')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Nilton G. Medeiros'
-    print(p.nome)
+    nilton = Pessoa(nome='Nilton')
+    matheus = Pessoa(nome='Matheus', idade=34)
+    print(Pessoa.cumprimentar(matheus))
+    print(id(matheus))
+    print(matheus.cumprimentar())
+    print(matheus.nome)
+    print(matheus.idade)
+    for filho in matheus.filhos:
+        print(filho.nome)
